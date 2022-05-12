1a10c2767e2e9175b1e29b8c9f2545a9abc18f35
Javier <javier@chewbacca.usc.edu>
2022-05-12 13:04:43 -0700

    Air 3D with uncertain wa first test

--- loss_functions.py
+++ loss_functions.py
@@ -121,6 +121,63 @@ def initialize_hji_air3D(dataset, minWith):
 
     return hji_air3D
 
+def initialize_hji_air3Dp1D(dataset, minWith):
+    # Initialize the loss function for the air3D problem
+    # The dynamics parameters
+    velocity = dataset.velocity
+    omega_max = dataset.omega_max
+    alpha_angle = dataset.alpha_angle
+
+    def hji_air3Dp1D(model_output, gt):
+        source_boundary_values = gt['source_boundary_values']
+        x = model_output['model_in']  # (meta_batch_size, num_points, 3+1+1)
+        y = model_output['model_out']  # (meta_batch_size, num_points, 1)
+        dirichlet_mask = gt['dirichlet_mask']
+        batch_size = x.shape[1]
+
+        du, status = diff_operators.jacobian(y, x)
+        dudt = du[..., 0, 0]
+        dudx = du[..., 0, 1:]
+
+        x_theta = x[..., 3] * 1.0
+        x_omega_a = x[..., 4] * 1.0
+
+        # Scale the costate for theta appropriately to align with the range of [-pi, pi]
+        dudx[..., 2] = dudx[..., 2] / alpha_angle
+        # Scale the coordinates
+        x_theta = alpha_angle * x_theta
+        x_omega_a = (x_omega_a * 1.5) + 4  #(2.5 to 5.5)
+
+        # Air3Dp1D dynamics
+        # \dot x    = -v_a + v_b \cos \psi + a y
+        # \dot y    = v_b \sin \psi - a x
+        # \dot \psi = b - a
+        # \dot \omega_a  = 0
+
+        # Compute the hamiltonian 
+        ham = x_omega_a * torch.abs(dudx[..., 0] * x[..., 2] - dudx[..., 1] * x[..., 1] - dudx[..., 2])  # Control component
+        ham = ham - omega_max * torch.abs(dudx[..., 2])  # Disturbance component
+        ham = ham + (velocity * (torch.cos(x_theta) - 1.0) * dudx[..., 0]) + (velocity * torch.sin(x_theta) * dudx[..., 1])  # Constant component
+
+        # If we are computing BRT then take min with zero
+        if minWith == 'zero':
+            ham = torch.clamp(ham, max=0.0)
+
+        if torch.all(dirichlet_mask):
+            diff_constraint_hom = torch.Tensor([0])
+        else:
+            diff_constraint_hom = dudt - ham
+            if minWith == 'target':
+                diff_constraint_hom = torch.max(diff_constraint_hom[:, :, None], y - source_boundary_values)
+
+        dirichlet = y[dirichlet_mask] - source_boundary_values[dirichlet_mask]
+
+        # A factor of 15e2 to make loss roughly equal
+        return {'dirichlet': torch.abs(dirichlet).sum() * batch_size / 15e2,
+                'diff_constraint_hom': torch.abs(diff_constraint_hom).sum()}
+
+    return hji_air3Dp1D
+
 
 def initialize_hji_drone2D(dataset, minWith):
     # Initialize the loss function for the drone2D problem
